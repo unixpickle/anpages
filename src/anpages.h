604f8c6a422efc6d7bbc9fd3e8f7821cc7631e3d
Alex Nichol <unixpickle@gmail.com>
2014-03-30 13:12:25 -0400

    separate anpages_struct_t

--- src/anpages.h
+++ src/anpages.h
@@ -5,7 +5,9 @@ typedef struct {
   uint64_t used;
   uint64_t list;
   uint64_t total;
-} __attribute__((packed)) * anpages_t;
+} __attribute__((packed)) anpages_struct_t;
+
+typedef anpages_struct_t * anpages_t;
 
 uint8_t anpages_initialize(anpages_t pages, uint64_t start, uint64_t total);
 uint64_t anpages_alloc(anpages_t pages);
